49491d1b4949e0eaa735ffa885fa0c1e9c965893
CalebeRios <calebe_rios@hotmail.com>
2018-10-23 21:50:21 -0300

    Adding initial config for banquinha project

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Provider } from 'react-redux';
+import promise from 'redux-promise';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+import App from './App';
+
+const store = createStore(reducers, applyMiddleware(promise));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'),
+);

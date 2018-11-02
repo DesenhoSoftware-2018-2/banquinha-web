b60c54778ad36df8b2d2693de3d57d3b41aaf20c
João Vitor <joaovytor0@gmail.com>
2018-11-02 20:30:24 -0300

    Adding datepicker and file upload
    Co-authored-by: Vitor Cardoso Xoteslem <vitorcx010@gmail.com>

--- src/views/Monitoria/create.js
+++ src/views/Monitoria/create.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import InputField from './inputField';
-
 class Create extends Component {
+
   render() {
     return(
       <div>
         <InputField type="text" placeholder="Nome:"/>
         <InputField type="text" placeholder="Conteudo:"/>
         <InputField type="text" placeholder="Tag:"/>
+        <InputField type="file" placeholder="Imagem"/>
+        <InputField type="datetime-local" placeholder="Data:"/>
       </div>
+
+
+       
     );
   }
 }

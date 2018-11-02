76eb3af5e188189b3aa463d6b3bf4ec70e43f1c8
Vítor Xoteslem <vitorcx010@gmail.com>
2018-11-02 19:25:54 -0300

    #73 Adding component route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Create from '../views/Monitoria/create';
 
 import HomePage from '../views/HomePage/homePage';
 
@@ -7,6 +8,7 @@ export default () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact render={props => <HomePage {...props} />} />
+      <Route path="/createMonitoria" exact render={props => <Create {...props} />} />
       {/* <Route path="/sobre" exact component={TimeLinePage} /> */}
     </Switch>
   </BrowserRouter>

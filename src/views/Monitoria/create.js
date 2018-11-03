48dc810028e4f68d5815372a3140fb3cf8212ee2
Vítor Xoteslem <vitorcx010@gmail.com>
2018-11-02 22:13:01 -0300

    #73 fixing input states control
    Co-authored-by: João Vitor Ramos de Souza <joaovytor0@gmail.com>

--- src/views/Monitoria/create.js
+++ src/views/Monitoria/create.js
@@ -1,19 +1,83 @@
 import React, { Component } from 'react';
 import InputField from './inputField';
+
 class Create extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      nome: '',
+      conteudo: '',
+      tag: '',
+      imagem: '',
+      data: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    console.log(event.target.name + 'value' + event.target.value)
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert(this.state.nome + 'And' +
+          this.state.conteudo +
+          this.state.tag +
+          this.state.imagem +
+          this.state.data + 'Was submited');
+    event.preventDefault();
+  }
+
   render() {
+
     return(
+
       <div>
-        <InputField type="text" placeholder="Nome:"/>
-        <InputField type="text" placeholder="Conteudo:"/>
-        <InputField type="text" placeholder="Tag:"/>
-        <InputField type="file" placeholder="Imagem"/>
-        <InputField type="datetime-local" placeholder="Data:"/>
-      </div>
+        <form onSubmit={this.handleSubmit} >
+          <InputField
+            name="nome"
+            type="text"
+            placeholder="Nome:"
+            value={this.state.nome}
+            handleChange={this.handleChange}
+          />
+          <InputField
+            name="conteudo"
+            type="text"
+            placeholder="Conteudo:"
+            value={this.state.conteudo}
+            handleChange={this.handleChange}
+          />
+          <InputField
+            name="tag"
+            type="text"
+            placeholder="Tag:"
+            value={this.state.tag}
+            handleChange={this.handleChange}
+          />
+          <InputField
+            name="imagem"
+            type="file"
+            placeholder="Imagem"
+            value={this.state.imagem}
+            handleChange={this.handleChange}
+          />
+          <InputField
+            name="data"
+            type="datetime-local"
+            placeholder="Data:"
+            value={this.state.data}
+            handleChange={this.handleChange}
+          />
+        <a className="waves-effect waves-light btn-small" onClick={this.handleSubmit}>Cadastrar</a>
+      </form>
+    </div>
 
 
-       
+
     );
   }
 }
--- src/views/Monitoria/inputField.js
+++ src/views/Monitoria/inputField.js
@@ -2,29 +2,11 @@ import React, { Component } from 'react';
 
 class InputField extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('A ' + this.state.value + 'Was submited');
-    event.preventDefault();
-  }
-
-
   render() {
     return(
       <label>
         {this.props.placeholder}
-        <input type={this.props.type} value={this.state.value} onChange={this.handleChange} />
+        <input name={this.props.name} type={this.props.type} onChange={this.props.handleChange} />
       </label>
     );
   }

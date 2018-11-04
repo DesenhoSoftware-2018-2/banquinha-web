b5d865e4208e42d0acfb20e89ef6443975df3be8
Amanda Muniz <amanda.muniiz@outlook.com>
2018-11-04 21:22:16 -0200

    #71 Creating basic handleChange and handleSubmit functions

--- src/views/usuario/cadastroForm.js
+++ src/views/usuario/cadastroForm.js
@@ -9,7 +9,18 @@ class CadastroForm extends Component {
       nome: '',
       email: '',
       senha: '',
+      senhaConfirmada: '',
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name] : event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
   }
 
   render() {
@@ -22,30 +33,31 @@ class CadastroForm extends Component {
               type = "text"
               placeholder = "Nome:"
               value = {this.state.nome}
-              // handleChange={this.handleChange}
+              handleChange={this.handleChange}
             />
             <CadastroInputForm
               name = "email"
               type = "text"
               placeholder = "Email:"
               value = {this.state.email}
-              // handleChange={this.handleChange}
+              handleChange={this.handleChange}
             />
             <CadastroInputForm
               name = "senha"
               type = "text"
               placeholder = "Senha:"
               value = {this.state.senha}
-              // handleChange={this.handleChange}
+              handleChange={this.handleChange}
             />
             <CadastroInputForm
               name = "confirmarSenha"
               type = "text"
               placeholder = "Confirmar senha:"
-              value = {this.state.senha}
-              // handleChange={this.handleChange}
+              value = {this.state.senhaConfirmada}
+              handleChange={this.handleChange}
             />
           </div>
+           <button className="waves-effect waves-light btn-small" onClick={this.handleSubmit}>Cadastrar</button>
         </form>
       </div>
     )

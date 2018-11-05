814bbdf11595bd7eb7cd44bb575198bd848fd1b5
MarthaDs <marthadntas@gmail.com>
2018-11-05 01:09:20 -0200

    Fix #72 creating links and star rating
    Co-authored-by: Calebe Rios <calebe_rios@hotmail.com>
    Co-authored-by: Bruno Dantas<oliveiradantas96@gmail.com@gmail.com>

--- src/components/sidenav/sidenav.js
+++ src/components/sidenav/sidenav.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import M from "materialize-css/dist/js/materialize.min.js"; 
 import "materialize-css/dist/css/materialize.css";
+import StarRatingComponent from 'react-star-rating-component';
 
-const img1 = require('../../assets/img/paisagem.jpeg');
-const img2 = require('../../assets/img/person.png');
+const background = require('../../assets/img/paisagem.jpeg');
+const userImage = require('../../assets/img/person.png');
 
 
 class SideNav extends Component{
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          rating: 0
+        };
+      }
+    
+      onStarClick(nextValue, prevValue, name) {
+        this.setState({rating: nextValue});
+      }
     
     componentDidMount(){
         var elems = document.querySelectorAll('.sidenav');
@@ -21,21 +33,37 @@ class SideNav extends Component{
                     <li>
                         <div className="user-view">
                             <div className="background">
-                                <img src={img1} />
+                                <img src={background} />
                             </div>
-                            <a href="#user"><img className="circle" src= {img2} /></a>
-                            <a href="#name"><span className="white-text name">John Doe</span></a>
-                            <a href="#email"><span className="white-text email">jdandturk@gmail.com</span></a>
+                            <a href="#user"><img className="circle" src= {userImage} /></a>
+                            <a href="#name"><span className="white-text name">name</span></a>
+                            <a href="#email"><span className="white-text email">email</span></a>
                         </div>
                     </li>
-                    <li><a href="#!"><i className="material-icons">cloud</i>First Link With Icon</a></li>
-                    <li><a href="#!">Second Link</a></li>
+                    
+                    <div className="center">
+                        <StarRatingComponent 
+                            name="star_rating" 
+                            starCount={5}
+                            value={this.state.rating}
+                            emptyStarColor="#d3d3d3"
+                            renderStarIcon={() => <span><i className="material-icons small">star</i></span>}
+                            onStarClick={this.onStarClick.bind(this)}
+                            editing={false}
+                        />
+                    </div>
                     <li>
                         <div className="divider"></div></li>
-                    <li><a className="subheader">Subheader</a></li>
-                    <li><a className="waves-effect" href="#!">Third Link With Waves</a></li>
+                    <li><a className="waves-effect" href="#!"><i className="material-icons">account_box</i>Conta</a></li>
+                    <li><a className="waves-effect" href="#!"><i className="material-icons">import_contacts</i>Histórico</a></li>
+                    <li><a className="waves-effect" href="#!"><i className="material-icons">assessment</i>Relatório</a></li>
+                    <li><a className="waves-effect" href="#!"><i className="material-icons">insert_comment</i>Conversa</a></li>
+                    <li><a className="waves-effect" href="#!"><i className="material-icons">notifications</i>Notificações</a></li>
+                    <li><a className="waves-effect" href="#!"><i className="material-icons">help</i>Ajuda</a></li>
+                    <li><a className="waves-effect" href="#!"><i className="material-icons">info</i>Sobre</a></li>
+                    <li><a className="waves-effect" href="#!"><i className="material-icons">exit_to_app</i>Sair</a></li>
                 </ul>
-                <a data-target="slide-out" className="sidenav-trigger sidenav-teste pulse waves-effect">  
+                <a data-target="slide-out" className="sidenav-trigger sidenav-button waves-effect">  
                     <i className="material-icons small">menu</i>         
                 </a>           
             </div>
